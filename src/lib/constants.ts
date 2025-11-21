e481c2220f09f9c780f29605a5553bd8ee77bad7
Jeon Sang Woo <tkddn1018@gmail.com>
2025-11-21 11:23:17 +0900

    버셀 보안 우회코드 작성

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,2 +1,5 @@
 // src/lib/constants.ts
-export const API_BASE_URL = "http://52.21.185.171";
+
+// HTTPS 우회해야해서 주석 걸어둠
+// export const API_BASE_URL = "http://52.21.185.171";
+export const API_BASE_URL = "";
