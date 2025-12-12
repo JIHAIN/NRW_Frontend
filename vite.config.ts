ee7961229bff9b8c7189806b1012c35c6f8cb7f4
Jeon Sang Woo <tkddn1018@gmail.com>
2025-12-12 15:58:58 +0900

    vite.config 문법 오류 수정

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss({ config: "./tailwind.config.js" })],
+  plugins: [react(), tailwindcss()],
   server: {
     host: "127.0.0.1",
     strictPort: true,
